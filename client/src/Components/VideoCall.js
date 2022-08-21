e679d6c20eea680791be29dd1eeeba5b456e55ee
IvanJimenezCodeHub <jimenezivan338@gmail.com>
2022-08-20 20:36:44 -0400

    Improved Dashboard UI & implemented the video call feature

---
+++ client/src/Components/VideoCall.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './VideoCall.css'
+
+export default function VideoCall() {
+    return (
+        <div>
+            <iframe src="https://linkstarter.whereby.com/demo-2281ef1e-5bbc-4094-b22c-1005740842f4" allow="camera; microphone; fullscreen; speaker; display-capture"></iframe>
+        </div>
+    )
+}
